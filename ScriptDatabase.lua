66b2818880a00aacac681f94f451867df3718977
Fadhil <fadhilakbarcariearsaindonesia@gmail.com>
2026-01-21 21:16:22 +0700

    Update Hub link in ScriptDatabase.lua

--- ScriptDatabase.lua
+++ ScriptDatabase.lua
@@ -2,4 +2,4 @@ return {
     -- [Command Name] = "Raw GitHub Link to Script"
     
     -- Example 1: Main Hub (Replace with your actual links)
-    ["Hub"] = "https://raw.githubusercontent.com/FadhilAkbarC/FadhilHub/main/scripts/Hub.lua"
+    ["Hub"] = "https://raw.githubusercontent.com/FadhilAkbarC/ExecutorDatabase/main/scripts/Hub.lua"
