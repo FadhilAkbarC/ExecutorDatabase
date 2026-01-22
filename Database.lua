fef3bd78acd285706912bd9e5c1a1bb84320038b
Fadhil <fadhilakbarcariearsaindonesia@gmail.com>
2026-01-22 16:57:30 +0700

    Update Database.lua

--- Database.lua
+++ Database.lua
@@ -1,5 +1,5 @@
 return {
     ["Idk"] = "https://raw.githubusercontent.com/User/Repo/main/scripts/fly.lua",
     ["esp"] = "https://raw.githubusercontent.com/User/Repo/main/scripts/esp.lua",
-    ["Hub"] = "https://raw.githubusercontent.com/FadhilAkbarC/ExecutorDatabase/main/scripts/Hub.lua"
+    ["Hub"] = "https://raw.githubusercontent.com/FadhilAkbarC/FadhilHubRoblox/main/scripts/Hub.lua"
 }
